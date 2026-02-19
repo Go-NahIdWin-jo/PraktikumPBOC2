78fe49c4c263cdb882a8ebca47c1c44b418c5a66
adel rayyan hakim <arayyanhakim@gmail.com>
2026-02-19 16:44:11 +0700

    add MTitik.java and Titik() Method

---
+++ Pertemuan1/src/MTitik.java
@@ -0,0 +1,14 @@
+/* Nama File : MTitik.java 
+ * Deskripsi : berisi atribut dan method dalam class Titik
+ * Pembuat : Adel Rayyan Hakim 24060124140173
+ * Tanggal : 19 Februari 2026
+ * 
+*/
+
+public class MTitik {
+	public static void main(String[] args) {
+		Titik T1 = new Titik(); //Membuat objek titik T1 (0,0)
+		System.out.println("Hello");
+		System.out.println(T1.absis);
+	}
+}
--- Pertemuan1/src/Titik.java
+++ Pertemuan1/src/Titik.java
@@ -2,5 +2,25 @@
  * Deskripsi : berisi atribut dan method dalam class Titik
  * Pembuat : Adel Rayyan Hakim 24060124140173
  * Tanggal : 19 Februari 2026
-/
+ * 
+*/
+
+/* PraktikumPBOC2 */
+/*     L IntroTest */
+/*     L Pertemuan1 */
+
+/* Menu, mirip dot h */
+
+public class Titik {
+    /* ATRIBUT */
+    double absis;
+    double ordinat;
+
+    /* METHOD */
+    //konstruktor untuk membuat titik (0,0)
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+    }
+}
 
