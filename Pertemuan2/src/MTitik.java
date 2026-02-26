a5ffb15db4eb386bed5e4429ee4127e1b8273ff0
Go/jo <arayyanhakim@gmail.com>
2026-02-26 16:21:36 +0700

    add new branch and java project Pertemuan2

---
+++ Pertemuan2/src/MTitik.java
@@ -0,0 +1,36 @@
+/* Nama File : MTitik.java 
+ * Deskripsi : file main titik berisi realisasi
+ * Pembuat : Adel Rayyan Hakim 24060124140173
+ * Tanggal : 26 Februari 2026
+ * 
+*/
+
+public class MTitik {
+	public static void main(String[] args) {
+		Titik T1 = new Titik(); // Membuat objek titik T1 (0,0)
+		System.out.println("Hello");
+		System.out.println(T1.absis);
+		System.out.println(T1.getAbsis());
+		System.out.println(T1.getOrdinat());
+		T1.setAbsis(6);
+		System.out.println(T1.getAbsis());
+		T1.setOrdinat(7);
+		System.out.println(T1.getOrdinat());
+		T1.geser(1, 1);
+		System.out.println(T1.getAbsis());
+		System.out.println(T1.getOrdinat());
+		T1.printTitik();
+
+		Titik T2 = T1;
+		T2.printTitik();
+		T1.setAbsis(3);
+		T1.setOrdinat(4);
+		T2.printTitik();
+
+		// overload, lihat parameter
+		Titik T3 = new Titik(8, 9);
+		System.out.println("\noverload\n");
+		T3.printTitik();
+
+	}
+}
---
+++ Pertemuan2/src/Titik.java
@@ -0,0 +1,64 @@
+/* Nama File : Titik.java 
+ * Deskripsi : berisi atribut dan method dalam class Titik
+ * Pembuat : Adel Rayyan Hakim 24060124140173
+ * Tanggal : 26 Februari 2026
+ * 
+*/
+
+public class Titik {
+    /* ATRIBUT */
+    double absis;
+    double ordinat;
+
+    /* METHOD */
+    // konstruktor untuk membuat titik (0,0)
+
+    Titik() {
+        absis = 0;
+        ordinat = 0;
+    }
+
+    // method Overload
+
+    Titik(double x, double y) {
+        absis = x;
+        ordinat = y;
+    }
+
+    // mengembalikan nilai absis
+
+    double getAbsis() {
+        return absis;
+    }
+
+    // mengembalikan nilai ordinat
+
+    double getOrdinat() {
+        return ordinat;
+    }
+
+    // mengubah nilai absis
+
+    void setAbsis(double x) {
+        absis = x;
+    }
+
+    // mengubah nilai ordinat
+
+    void setOrdinat(double y) {
+        ordinat = y;
+    }
+
+    // menggeser nilai absis dan ordinat
+
+    void geser(double x, double y) {
+        absis = absis + x;
+        ordinat = ordinat + y;
+    }
+
+    // mencetak absis dan ordinat
+
+    void printTitik() {
+        System.out.println("Titik (" + absis + "," + ordinat + ")");
+    }
+}
