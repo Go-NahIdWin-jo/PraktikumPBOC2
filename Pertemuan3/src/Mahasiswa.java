556ddf846d084dabe6637d0318eaec7b4e82003c
Go/jo <arayyanhakim@gmail.com>
2026-03-12 14:50:27 +0700

    done. lengkap

--- Pertemuan3/src/Mahasiswa.java
+++ Pertemuan3/src/Mahasiswa.java
@@ -33,6 +33,14 @@ public class Mahasiswa {
         this.prodi = prodi;
     }
 
+    public void setDosenWali(Dosen dosenWali) {
+        this.dosenWali = dosenWali;
+    }
+
+    public void setKendaraan(Kendaraan kendaraan) {
+        this.kendaraan = kendaraan;
+    }
+
     // mutator
 
     public String getNama() {
@@ -45,6 +53,47 @@ public class Mahasiswa {
 
     public String getProdi() {
         return prodi;
+    }
+
+    public void addMatkul(MataKuliah newMatkul) {
+        listMatKul.add(newMatkul);
+    }
+
+    public int getJumlahMatkul() {
+        return listMatKul.size();
+    }
+
+    public int getJumlahSKS() {
+        int i;
+        int jumlahSKS;
+        jumlahSKS = 0;
+        for (i = 0; i < listMatKul.size(); i++) {
+            jumlahSKS = jumlahSKS + listMatKul.get(i).getSks();
+        }
+        return jumlahSKS;
+    }
+
+    public void printMhs() {
+        System.out.println("Nim : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Prodi : " + prodi);
+    }
+
+    public void printDetailMhs() {
+        System.out.println("Nim : " + nim);
+        System.out.println("Nama : " + nama);
+        System.out.println("Prodi : " + prodi);
+        int i;
+        System.out.println("Daftar Matkul : ");
+        for (i = 0; i < listMatKul.size(); i++) {
+            System.out.println(listMatKul.get(i).getNama());
+        }
+        System.out.println("end");
+        System.out.println("Nama Dosen Wali : " + dosenWali.getNama());
+        System.out.println("Nip Dosen Wali : " + dosenWali.getNip());
+        System.out.println("Prodi Dosen Wali : " + dosenWali.getProdi());
+        System.out.println("Kendaraan Mahasiswa : " + kendaraan.getJenis());
+        System.out.println("Plat Nomor Kendaraan Mahasiswa : " + kendaraan.getNoPlat());
     }
 
 }
